2b4e94d5b64a8e2745224951856247febbd91806
mariaines20397 <69004580+mariaines20397@users.noreply.github.com>
2023-07-21 08:41:45 -0300

    modificaciones visuales en perfil de usuario

--- src/app/pages/main/user/user.component.ts
+++ src/app/pages/main/user/user.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { User } from './model/users.model';
+import * as UserActions from './store/user.actions';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
+  datosUsuario:any[]=[
+    {
+      name:'María Inés Toledo',
+      usuario:'mariainestoledo',
+      email:'mariainestoledo20397@gmail.com',
+      telefono:3804590089,
+      documento:39905178
+    }
+  ]
+  
+  constructor(
+    private store:Store<{user:User}>
+  ){}
+  ngOnInit(): void {
+    this.store.dispatch(UserActions.loadUser());
+  }
 
 }
