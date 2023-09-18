8f03ababa6b9c6d7ccb8eb53186452eb2c8c9ced
mariaines20397 <69004580+mariaines20397@users.noreply.github.com>
2023-09-18 19:41:43 -0300

    fix en admin.module

---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: 'admin.component.html',
+  styleUrls: ['admin.component.css']
+})
+export class AdminComponent {
+ 
+}
--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminComponent } from './admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeAdminComponent } from './homeAdmin/homeAdmin.component';
 import { TableProductsComponent } from './table-products/table-products.component';
 import { TableUsersComponent } from './table-users/table-users.component';
+import { AdminComponent } from './admin.component';
 
 
 
