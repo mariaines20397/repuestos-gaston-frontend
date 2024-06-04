6c8d67d43cf37c032908a565409c029fd63c646e
mariaines20397 <mariainestoledo20397@gmail.com>
2024-06-03 22:07:36 -0300

    home admin

--- src/app/pages/admin/homeAdmin/homeAdmin.component.ts
+++ src/app/pages/admin/homeAdmin/homeAdmin.component.ts
@@ -1,29 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminProductsService } from '../table-products/services/admin-products.service';
-import { Product } from '../table-products/model/product.model';
-import { User } from '../table-users/model/users.model';
+import { Product, getAllProduct } from '../table-products/model/product.model';
+import { User, getAllUser } from '../table-users/model/users.model';
 import { AdminUsersService } from '../table-users/services/admin-users.service';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import * as UserAdminActions from '../table-users/store/users.actions'
+import * as ProductosAdminActions from '../table-products/store/products.actions';
 
 @Component({
   templateUrl: 'homeAdmin.component.html',
   styleUrls: ['homeAdmin.component.css']
 })
 export class HomeAdminComponent implements OnInit{
-  productos:Product[]=[]
-  users:User[]=[]
+  productAdmin:any={}
+  userAdmin:any={}
+  productsStockLow:any[]=[]
+  private subscriptions = new Subscription();
+
   constructor(
-    private productServices:AdminProductsService,
-    private userServices:AdminUsersService
-  ) { }
-  ngOnInit(): void {
-    this.getProducts()
-    this.getUsers()
+    private store:Store<{ userAdmin:getAllUser, productAdmin: getAllProduct}>,
+  ) { 
+    this.subscriptions.add(
+      this.store
+      .select('productAdmin')
+      .subscribe((productAdmin) => {
+          this.productAdmin = productAdmin;
+          this.calculateStockSlow(this.productAdmin)
+      })
+    );
+    this.subscriptions.add(
+      this.store
+      .select('userAdmin')
+      .subscribe((userAdmin) => {
+          this.userAdmin = userAdmin;
+      })
+    );
+    
   }
-  getProducts(){
-    this.productos=this.productServices.getProducts();
+  ngOnInit(): void {
+    this.store.dispatch(UserAdminActions.loadUsers({pageable:undefined}));
+    this.store.dispatch(ProductosAdminActions.loadProducts({pageable:undefined}));
+
   }
 
-  getUsers(){
-    this.users=this.userServices.getUsers();
+  calculateStockSlow(products:any){
+    products.data.forEach((data:any) => {
+      if (data.stock <= 5) {
+        this.productsStockLow.push(data);
+      }
+    });
   }
 }
